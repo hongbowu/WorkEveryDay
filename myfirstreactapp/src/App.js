b5d9ed4455e93fbc55476a5a353bf063ac5874fe
hongbowu <wuhongbo87@gmail.com>
2024-03-21 19:25:02 -0400

    try to render the web with props

--- myfirstreactapp/src/App.js
+++ myfirstreactapp/src/App.js
@@ -1,28 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import GroceryItem from './GroceryItem'
+import React from 'react';
+import {comments} from './commentData'
+import Card from './Card'
+import './App.css'
 
-function App() {
-  function handleClick(name) {
-    alert(name + ' has been added to the cart!')
-  }
-  const groceries = ['Eggs','Banana', 'Strawberry', 'Bread'];
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {groceries.map(grocery => <GroceryItem key={grocery} onClick={()=> handleClick(grocery)} name={grocery} />)}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+ return (
+  comments.map((comment,index) => <Card key= {index} commentObject= {comment} />)
+ )
+    
+   
 }
 
 export default App;
--- myfirstreactapp/src/GroceryItem.jsx
+++
@@ -1,8 +0,0 @@
-
-function GroceryItem(props) {
-    return <button onClick={props.onClick} style={{backgroundColor: 'green', color: 'white', borderRadius: '7px'}}>
-        {props.name}
-        </button>
-}
-
-export default GroceryItem;
