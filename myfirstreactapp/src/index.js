600a294cffd63bb8f472e1d36e3fdee17680bfb6
hongbowu <wuhongbo87@gmail.com>
2024-04-15 11:14:53 -0400

    test the code and fix the issues

--- myfirstreactapp/src/index.js
+++ myfirstreactapp/src/index.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {animals} from './animals';
+
+const title = '';
+const images = [];
+for (const animal in animals) {
+  images.push(
+    <img
+      key={animal}
+      className="animal"
+      alt={animal}
+      src={animals[animal].image}
+      arial-label={animal}
+      role="button"
+      onClick={displayFact}
+    />
+  );
+}
+function displayFact(e) {
+  let selectedAnimal = e.target.alt;
+  let fact = animals[selectedAnimal].facts[Math.floor(Math.random()*animals[selectedAnimal].facts.length)]
+  document.getElementById('fact').innerHTML = fact;
+}
+
+const background = (
+  <img className="background" alt="ocean" src="/images/ocean.jpg" />
+);
+
+
+const showBackground = true;
+const animalFacts = (
+  <div>
+    <h1>{title ? title : "Click an animal for a fun fact"}</h1>
+    {showBackground === true && background}
+    <div className="animals">{images}</div>
+    <p id='fact'></p>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+  {animalFacts}
+  </>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
